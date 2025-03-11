02cca7d45e330096542e25a6e06a22c618fa5a24
Haryce <148765923+Haryce@users.noreply.github.com>
2025-03-11 09:14:57 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int* doubleArray(const int* arr, int size) {
+    int* nArr = new int[size];
+    for (int i = 0; i < size; i++) {
+        nArr[i] = arr[i] * 2;
+    }
+    return nArr; 
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    const int size = 5;
+    int* arr = new int[size];
+    cout << "Введите " << size << " целых чисел:\n";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    int* arr1 = doubleArray(arr, size);
+    cout << "Новый массив (удвоенные значения): ";
+    for (int i = 0; i < size; i++) {
+        cout << arr1[i] << " ";
+    }
+    delete[] arr;
+    delete[] arr1;
+    return 0;
+}
